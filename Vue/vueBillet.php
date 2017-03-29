6f6d71dc03f67062de02b4dbf037ce61e0157d40
berthelot <nberthelot29@gmail.com>
2017-03-29 17:06:11 +0200

    Passage à une architecture MVC orienté objet

--- Vue/vueBillet.php
+++ Vue/vueBillet.php
@@ -1,6 +1,6 @@
 <?php $titre = "Mon Blog - " . $billet['titre']; ?>
 
-<?php ob_start(); ?>
+
 <article>
     <header>
         <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
@@ -16,6 +16,12 @@
     <p><?= $commentaire['auteur'] ?> dit :</p>
     <p><?= $commentaire['contenu'] ?></p>
 <?php endforeach; ?>
-<?php $contenu = ob_get_clean(); ?>
 
-<?php require 'gabarit.php'; ?>
+<form method="post" action="index.php?action=commenter">
+    <input id="auteur" name="auteur" type="text" placeholder="Votre pseudo" 
+           required /><br />
+    <textarea id="txtCommentaire" name="contenu" rows="4" 
+              placeholder="Votre commentaire" required></textarea><br />
+    <input type="hidden" name="id" value="<?= $billet['id'] ?>" />
+    <input type="submit" value="Commenter" />
+</form>
